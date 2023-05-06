18db8a826c2cfacf3698e586b579ff6028658be9
profikiller-zzy <3548361574@qq.com>
2023-05-06 14:50:59 +0800

    项目搭建+Andt引入

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/admin/admin.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="gvb_admin">
+    <aside>
+      <div class="icon-caidan"></div>
+
+    </aside>
+
+    <div class="main">
+      <header>
+
+      </header>
+      <div class="tabs">
+
+      </div>
+
+      <main></main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.gvb_admin{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+
+  aside{
+    flex-basis: 240px;
+    height: 100vh;
+    background-color: grey;
+  }
+
+  .main{
+    width: calc(100% - 240px);
+
+    header{
+      height: 60px;
+      background-color: aquamarine;
+    }
+
+    .tabs{
+      height: 30px;
+      background-color: white;
+    }
+
+    main {
+      background-color: #f0eeee;
+      height: calc(100vh - 90px);
+    }
+
+  }
+}
+</style>
